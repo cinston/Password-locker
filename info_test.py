5bd5b9171de031316475927c0bd44850ec54df17
Winston <wagawc@gmail.com>
2018-11-05 09:49:30 +0300

    test save to test if the password is saved into the passlock list

--- info_test.py
+++ info_test.py
@@ -12,4 +12,26 @@ def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_cred = Credential("Carlos Stone","Facebook", "Winston", "qwerti98")
+        self.new_info = Info("Carlos Stone","Facebook", "Winston", "qwerti98")
+def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Info.info_list = []
+
+def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_info.username,"Carlos Stone")
+        self.assertEqual(self.new_info.info_app,"Facebook")
+        self.assertEqual(self.new_info.info_user,"Winston")
+        self.assertEqual(self.new_info.info_pass,"qwerti98") 
+ def test_save_info(self):
+        '''
+        test_save_contact test case to test if the passlock object is saved into
+         the passlock list
+        '''
+        self.new_info.save_info() # saving the new info
+        self.assertEqual(len(Info.info_list),1)
