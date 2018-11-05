c39e23345a06e1848ba7fcfb10a15ad496dc70e1
Winston <wagawc@gmail.com>
2018-11-05 08:23:37 +0300

    set up a method to run before each test case

--- info_test.py
+++ info_test.py
@@ -8,3 +8,8 @@ class TestInfo(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     """
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_cred = Credential("Carlos Stone","Facebook", "Winston", "qwerti98")
