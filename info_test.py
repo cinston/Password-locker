dbdae59ed982f8012fa93f7022f092ebd4b88ab2
Winston <wagawc@gmail.com>
2018-11-05 08:05:32 +0300

    Add unittest to help create test cases

---
+++ info_test.py
@@ -0,0 +1,10 @@
+import unittest     #import unittest module
+from info import Info   #import the user class
+
+
+class TestInfo(unittest.TestCase):
+    """
+    Test class that defines test cases for the Info class behavioursself.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
