0e5ec604f9c8f01b45eeb098390b6b77420ac1d9
Winston <wagawc@gmail.com>
2018-11-05 08:00:17 +0300

    define properties for our objects

---
+++ info.py
@@ -0,0 +1,45 @@
+ class Info:
+    """
+    Class that generates new instances of users
+    """
+
+    pass
+     def __init__(self, username, cred_app, cred_user, cred_pass):
+        """
+        __init__ method that helps us define properties for our objects.
+        """
+        self.username = username
+        self.info_app = info_app
+        self.info_user = info_user
+        self.info_pass = info_pass
+        def save_cred(self):
+        '''
+        save_info method saves info objects into info_list
+        '''
+
+#         Info.cred_list.append(self)
+#  @classmethod
+#     def search_info(cls, user_search, app_search):
+#         '''
+#         Method that takes in an Appname and username and returns a info that matches.
+#         Args:
+#             username: Username to search for
+#             info_app: Appname to search
+#         Returns :
+#             Info that matches Username and Appname.
+#         '''
+
+#         for info in cls.info_list:
+#             if info.username == user_search and info.cred_app == app_search:
+#                 return info      
+
+#  @classmethod
+#     def display_info(cls, user_search):
+#         '''
+#         method that returns the info list
+#         '''
+#         all_user_info =[]
+#         for info in cls.info_list:
+#             if info.username == user_search:
+#                 all_user_info.append(info)
+#         return all_user_info              
